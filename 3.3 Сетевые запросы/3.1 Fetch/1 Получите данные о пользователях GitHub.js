ef34a72ceb26b9cd5e268a34344a34b7f7d7b08e
Tatiana Tsogoeva <tsogoevats@gmail.com>
2023-03-12 22:42:56 +0700

    cover commit

--- 3.3 Сетевые запросы/3.1 Fetch/1 Получите данные о пользователях GitHub.js
+++ 3.3 Сетевые запросы/3.1 Fetch/1 Получите данные о пользователях GitHub.js
@@ -1,7 +1,7 @@
-async function getUsers(names) {
-	let results = await Promise.all(names.map((name) => fetch(`https://api.github.com/users/${name}`)));
+// async function getUsers(names) {
+// 	let results = await Promise.all(names.map((name) => fetch(`https://api.github.com/users/${name}`)));
 
-	return await Promise.all(results.map((response) => {
-		return response.ok ? response.json() : null;
-	}));
-}
+// 	return await Promise.all(results.map((response) => {
+// 		return response.ok ? response.json() : null;
+// 	}));
+// }
